8bdb114df890f0827c5d66da0841da67470a5db2
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2025-04-12 17:55:10 +0300

    Add Scrutor for improved DI and EF Core setup
    Updated `CleanArchitecture.Infrastructure.csproj` to include
    `Scrutor` version `6.0.1` for assembly scanning and
    dependency injection. Introduced `InfrastructureRegistrar`
    class with `AddInfrastructure` method to configure
    Entity Framework Core's database context using a SQL Server
    connection string. Utilized `Scrutor` for automated service
    registration, enhancing the DI setup.

---
+++ CleanArchitecture.Infrastructure/InfrastructureRegistrar.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitecture.Domain.Employees;
+using CleanArchitecture.Infrastructure.Context;
+using CleanArchitecture.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class InfrastructureRegistrar
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                string connectionString = configuration.GetConnectionString("SqlServer")!;
+                options.UseSqlServer(connectionString);
+            });
+
+            //services.AddScoped<IEmployeeRepository, EmployeeRepository>(); bu şekilde yazmak yerine aşağıdaki gibi yazabiliriz. --Scrutor
+            services.Scan(opt => opt
+            .FromAssemblies(typeof(InfrastructureRegistrar).Assembly)
+            .AddClasses(publicOnly: false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            );
+
+
+
+            return services;
+
+
+        }
+    }
+}
