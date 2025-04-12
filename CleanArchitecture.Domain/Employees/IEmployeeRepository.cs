13d363b9448a4ff001d06db0652584bafdf1111a
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2025-04-12 14:59:24 +0300

    Add IEmployeeRepository and EF Core package reference
    Updated `CleanArchitecture.Domain.csproj` to include
    `TS.EntityFrameworkCore.GenericRepository` version `9.0.1`
    for implementing a generic repository pattern.
    
    Added `IEmployeeRepository` interface with methods for
    managing `Employee` entities, enhancing data access
    and management capabilities.

---
+++ CleanArchitecture.Domain/Employees/IEmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Employees
+{
+    public interface IEmployeeRepository : IRepository<Employee>
+    {
+        Task<Employee?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task AddAsync(Employee employee);
+        Task UpdateAsync(Employee employee);
+        Task DeleteAsync(Guid id);
+    }
+    {
+
+    }
+}
